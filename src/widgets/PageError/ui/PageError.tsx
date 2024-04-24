911a00e2bec6b3bae0b9d937cdeac0411ad0c6da
AlekFletch <darren7@rambler>
2024-04-24 15:48:14 +0300

    lesson_27 fix checks 4

--- src/widgets/PageError/ui/PageError.tsx
+++ src/widgets/PageError/ui/PageError.tsx
@@ -1,8 +1,8 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { Button } from 'shared/ui/Button/Button';
-import cls from './PageError.module.scss';
 import React from 'react';
+import cls from './PageError.module.scss';
 
 interface PageErrorProps {
     className?: string;
