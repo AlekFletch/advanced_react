6290d6fab122d7e618d8a69cd792354c88d5702b
AlekFletch <darren7@rambler>
2024-04-24 14:32:19 +0300

    lesson_27 github_actions

---
+++ config/storybook/preview.ts
@@ -0,0 +1,18 @@
+import type { Preview } from '@storybook/react';
+import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
+import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
+
+const preview: Preview = {
+    parameters: {
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
+    },
+};
+
+export default preview;
+
+export const decorators = [StyleDecorator, RouterDecorator];
