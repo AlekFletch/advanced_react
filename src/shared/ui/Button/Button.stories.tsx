419b111ac857039d3942a840ed487d55591644a2
AlekFletch <darren7@rambler>
2024-05-03 16:37:54 +0300

    lesson_28 upgrade sidebar, add button themes

--- src/shared/ui/Button/Button.stories.tsx
+++ src/shared/ui/Button/Button.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import React from 'react';
-import { Button, ThemeButton } from './Button';
+import { Button, ButtonSize, ButtonTheme } from './Button';
 import { ThemeDecorator } from '../../config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from '../../../app/providers/ThemeProvider';
 
@@ -28,7 +28,7 @@ export const Clear: StoryObj<typeof Button> = {
     ...Template,
     args: {
         children: 'Text',
-        theme: ThemeButton.CLEAR,
+        theme: ButtonTheme.CLEAR,
     },
     decorators: [ThemeDecorator(Theme.LIGHT)],
 };
@@ -36,16 +36,79 @@ export const Outline: StoryObj<typeof Button> = {
     ...Template,
     args: {
         children: 'Text',
-        theme: ThemeButton.OUTLINE,
+        theme: ButtonTheme.OUTLINE,
     },
     decorators: [ThemeDecorator(Theme.LIGHT)],
 };
 
+export const OutlineSizeL: StoryObj<typeof Button> = {
+    ...Template,
+    args: {
+        children: 'Text',
+        theme: ButtonTheme.OUTLINE,
+        size: ButtonSize.L,
+    },
+    decorators: [ThemeDecorator(Theme.LIGHT)],
+};
+export const OutlineSizeXL: StoryObj<typeof Button> = {
+    ...Template,
+    args: {
+        children: 'Text',
+        theme: ButtonTheme.OUTLINE,
+        size: ButtonSize.XL,
+    },
+    decorators: [ThemeDecorator(Theme.LIGHT)],
+};
 export const OutlineDark: StoryObj<typeof Button> = {
     ...Template,
     args: {
         children: 'Text',
-        theme: ThemeButton.OUTLINE,
+        theme: ButtonTheme.OUTLINE,
     },
     decorators: [ThemeDecorator(Theme.DARK)],
+};
+
+export const BackgroundTheme: StoryObj<typeof Button> = {
+    ...Template,
+    args: {
+        children: 'Text',
+        theme: ButtonTheme.BACKGROUND,
+    },
+};
+
+export const BackgroundInverted: StoryObj<typeof Button> = {
+    ...Template,
+    args: {
+        children: 'Text',
+        theme: ButtonTheme.BACKGROUND_INVERTED,
+    },
+};
+
+export const Square: StoryObj<typeof Button> = {
+    ...Template,
+    args: {
+        children: '>',
+        theme: ButtonTheme.BACKGROUND_INVERTED,
+        square: true,
+    },
+};
+
+export const SquareSizeL: StoryObj<typeof Button> = {
+    ...Template,
+    args: {
+        children: '>',
+        theme: ButtonTheme.BACKGROUND_INVERTED,
+        square: true,
+        size: ButtonSize.L,
+    },
+};
+
+export const SquareSizeXL: StoryObj<typeof Button> = {
+    ...Template,
+    args: {
+        children: '>',
+        theme: ButtonTheme.BACKGROUND_INVERTED,
+        square: true,
+        size: ButtonSize.XL,
+    },
 };
