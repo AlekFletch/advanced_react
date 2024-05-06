e488fda9f79e72bdaba0487ea11aab65d1a9dbe0
AlekFletch <darren7@rambler>
2024-05-06 12:15:57 +0300

    fix ts/scss errors

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { fireEvent, screen } from '@testing-library/react';
 import { Sidebar } from 'widgets/Sidebar';
-import {componentRender} from "../../../../shared/lib/tests/componentRender/componentRender";
+import { componentRender } from '../../../../shared/lib/tests/componentRender/componentRender';
 
 describe('Sidebar', () => {
     test('with only first param', () => {
