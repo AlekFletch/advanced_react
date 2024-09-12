678358c1500bd8cb123b56cc0b4afd819b376148
AlekFletch <darren7@rambler>
2024-09-12 14:19:21 +0300

    Add Input, LoginForm, LoginModal (mw)

---
+++ src/entities/User/index.ts
@@ -0,0 +1,9 @@
+export {
+    userReducer,
+    userActions,
+} from './model/slice/userslice';
+
+export {
+    UserSchema,
+    User,
+} from './model/types/user';
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { Input } from 'shared/ui/Input/Input';
 
 const MainPage = () => {
     const { t } = useTranslation();
-
+    const [value, setValue] = React.useState('');
+    const onChange = (val: string) => {
+        setValue(val);
+    };
     return (
         <div>
             {t('Главная страница')}
+            <Input
+                placeholder='Введите текст'
+                value={value}
+                onChange={onChange}
+            />
         </div>
     );
 };
